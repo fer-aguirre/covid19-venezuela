96fe544e38ac3edfd716efd64b115d323ee8f14f
Fer Aguirre <kay_op@protonmail.com>
2022-08-11 20:18:21 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Covid19 Venezuela',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Análisis de datos de muertes por covid-19 en Venezuela',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
